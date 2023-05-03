d388c24b3d7197c384a6af09238cd309bf2732b8
José Henrique R. Borges <joseh202017@gmail.com>
2023-05-03 17:58:20 -0300

    app clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
 
-export default App;
+  return (
+    <div className='app-window'>
+      <div className='header'>
+        HEADER
+      </div>
+      <div className='body'>
+        BODY
+      </div>
+      <div className='footer'>
+        FOOTER
+      </div>
+    </div>
+  )
+}
