882ddf4469e12df16d50d74caad91f91ac424f4b
José Henrique R. Borges <joseh202017@gmail.com>
2023-05-04 16:08:12 -0300

    app upload

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,24 +2,41 @@ import React from "react";
 import './index.css';
 
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default () => {
 
     return (
         <div className='app-header'>
+            <div className="conteiner">
+                <div className='logo'>
+                    <img 
+                        className="imglogo"
+                        src="https://cdn-icons-png.flaticon.com/512/3209/3209945.png"
+                        width={'75px'}
+                        height={'75px'}
+                    />
+                </div>
 
-            <div className='logo'>
-                Logo
+                <div className='cartAndLogin'>
+                    <div className="cart">
+                        <ShoppingCartOutlinedIcon 
+                            style={{
+                                width: '50px',
+                                height: '50px'
+                            }}
+                        />
+                    </div>
+                    <div className="login">
+                        <AccountCircleIcon
+                            style={{
+                                width: '50px',
+                                height: '50px'
+                            }}
+                        />
+                    </div>
+                </div>
             </div>
-
-            <div className='cart'>
-                <ShoppingCartOutlinedIcon 
-                    style={{
-                        width: '35px',
-                        height: '35px'
-                    }}/>
-            </div>
-
         </div>
     );
 }
--- src/components/mainContent/index.js
+++ src/components/mainContent/index.js
@@ -3,32 +3,74 @@ import './index.css';
 
 export default () => {
     return (
-      <div className='app-body'>
-        
-          <div className='app-conteiner'>
+        <div className='app-body'>
 
-            <div className='side-categories'>
+            <div className="system-msg">mensagem de boas vindas</div>
 
-              <div className='sc'>sc1</div>
-              <div className='sc'>sc2</div>
-              <div className='sc'>sc3</div>
-              <div className='sc'>sc4</div>
+            <div className='app-conteiner'>
+
+                <div className='side-categories'>
+
+                    <div className='sc'>sideCategories1</div>
+                    <div className='sc'>sideCategories2</div>
+                    <div className='sc'>sideCategories3</div>
+                    <div className='sc'>sideCategories4</div>
+                    <div className='sc'>sideCategories5</div>
+
+                </div>
+
+                <div className='app-hots'>
+                    promo/hots
+                </div>
 
             </div>
 
-            <div className='app-promotions'>
-              promo
+            <div className="main-section">
+
+                <div className='sections'>
+
+                    <div className="content">
+                        1
+                    </div>
+
+                    <div className="content">
+                        2
+                    </div>
+
+                    <div className="content">
+                        3
+                    </div>
+                    <div className="content">
+                        4
+                    </div>
+                    <div className="content">
+                        5
+                    </div>
+                </div>
+
+                <div className='sections'>
+
+                    <div className="content">
+                        6
+                    </div>
+
+                    <div className="content">
+                        7
+                    </div>
+
+                    <div className="content">
+                        8
+                    </div>
+                    <div className="content">
+                        9
+                    </div>
+                    <div className="content">
+                        10
+                    </div>
+                </div>
+                
             </div>
 
-          </div>
-
-          <div className='app-contents'>
-            <div className="content">1</div>
-            <div className="content">2</div>
-            <div className="content">3</div>
-            <div className="content">4</div>
-          </div>
-
-      </div>
+        </div>
     );
 }
