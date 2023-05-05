12b0b878910523e872bbf7605a377c7b27eab11a
José Henrique R. Borges <joseh202017@gmail.com>
2023-05-05 18:18:48 -0300

    changing folders and body code
    body change for better application scalability in the future.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,109 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/header/index.js';
-import Body from './components/appBody/index.js';
 import Footer from './components/footer/index.js';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import ProductDefault from "./components/productDefault/index";
+import ProductHots from "./components/productHots/index";
 
 export default () => {
 
   return (
+
     <div className='app-window'>
       
-      <Header />
+        <Header />
 
-      <Body />
+        <div className='app-body'>
 
-      <Footer />
+            <div className="system-msg">mensagem de boas vindas(opcional)</div>
 
+                    <div className='app-conteiner'>
+
+                        <div className="hots-window">
+                            
+                            <div className="title-hots">
+                                <h1>Hots/Promotions</h1>
+                            </div>
+
+                            <div className='products-hots'>
+
+                                    <ProductHots />
+
+                                    <ProductHots />
+
+                                    <ProductHots />
+
+                                    <ProductHots />
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                        <div className='side-categories'>
+
+                            <div className="sc-header">
+                                <div className="sc-icon">
+                                    <FormatListBulletedIcon
+                                        style={{
+                                            width: '40px',
+                                            Height: '40px'
+                                        }}
+                                    />
+                                </div>
+                                <div className='sc-title'>
+                                    Categorias
+                                </div>
+                            </div>
+
+                            <div className="sc-content">
+                                <div className='sc'>sideCategories2</div>
+                                <div className='sc'>sideCategories3</div>
+                                <div className='sc'>sideCategories4</div>
+                                <div className='sc'>sideCategories5</div>
+                                <div className='sc'>sideCategories6</div>
+                                <div className='sc'>sideCategories7</div>
+                                <div className='sc'>sideCategories8</div>
+                            </div>
+
+                        </div>
+
+                    <div className="main-section">
+
+                        <div className='sections'>
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                        </div>
+
+                        <div className='sections'>
+                            
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                            <ProductDefault />
+
+                        </div>
+                    
+                    </div>
+
+            </div>
+
+        <Footer />
     </div>
   )
 }
--- src/components/appBody/index.js
+++ src/components/appBody/index.js
@@ -1,99 +1,40 @@
 import React from "react";
-import './index.css';
+import './style.css';
 
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import ProductDefault from "../productDefault/index";
-import ProductHots from "../productHots";
 
 export default () => {
 
     
     return (
-        <div className='app-body'>
 
-            <div className="system-msg">mensagem de boas vindas(opcional)</div>
+        <div className='side-categories'>
 
-            <div className='app-conteiner'>
-
-                <div className='side-categories'>
-
-                    <div className="sc-header">
-                        <div className="sc-icon">
-                            <FormatListBulletedIcon
-                                style={{
-                                    width: '40px',
-                                    Height: '40px'
-                                }}
-                            />
-                        </div>
-                        <div className='sc-title'>
-                            Categorias
-                        </div>
-                    </div>
-
-                    <div className="sc-content">
-                        <div className='sc'>sideCategories2</div>
-                        <div className='sc'>sideCategories3</div>
-                        <div className='sc'>sideCategories4</div>
-                        <div className='sc'>sideCategories5</div>
-                        <div className='sc'>sideCategories6</div>
-                        <div className='sc'>sideCategories7</div>
-                        <div className='sc'>sideCategories8</div>
-                    </div>
-
+            <div className="sc-header">
+                <div className="sc-icon">
+                    <FormatListBulletedIcon
+                        style={{
+                            width: '40px',
+                            Height: '40px'
+                        }}/>
                 </div>
 
-                <div className="hots-window">
-                    <div className="title-hots">
-                        <h1>Hots/Promotions</h1>
-                    </div>
-                    <div className='products-hots'>
-
-                            <ProductHots />
-
-                            <ProductHots />
-
-                            <ProductHots />
-
-                            <ProductHots />
-
-                    </div>
+                <div className='sc-title'>
+                    Categorias
                 </div>
-
             </div>
 
-            <div className="main-section">
-
-                <div className='sections'>
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                </div>
-
-                <div className='sections'>
-                    
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                    <ProductDefault />
-
-                </div>
-               
+            <div className="sc-content">
+                <div className='sc'>sideCategories2</div>
+                <div className='sc'>sideCategories3</div>
+                <div className='sc'>sideCategories4</div>
+                <div className='sc'>sideCategories5</div>
+                <div className='sc'>sideCategories6</div>
+                <div className='sc'>sideCategories7</div>
+                <div className='sc'>sideCategories8</div>
             </div>
 
         </div>
+
     );
 }
